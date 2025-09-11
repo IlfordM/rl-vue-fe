aeb5d190ef1b1cdd580725d876a5c445d3bfef58
Viachaslau Sokal <viachaslau.sokal@innowise.com>
2025-09-11 08:51:47 +0200

    Add Icon component and color variables for theming

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,49 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Icon from "./components/Icon.vue"
+</script>
 
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <nav class="navigation">
+      <router-link to="/">
+        <Icon name="home" class="nav-icon" />
+        Home
+      </router-link>
+      <router-link to="/about">
+        <Icon name="user" class="nav-icon" />
+        About
+      </router-link>
+      <router-link to="/contacts">
+        <Icon name="mail" class="nav-icon" />
+        Contacts
+      </router-link>
     </nav>
     <router-view />
   </div>
 </template>
 
 <style scoped>
-nav {
-  padding: 30px;
-  text-align: center;
+.navigation {
+  padding: 12px;
+  text-align: end;
 }
 
-nav a {
+.navigation a {
   font-weight: bold;
   color: #2c3e50;
-  text-decoration: none;
+  text-decoration: underline;
   margin: 0 10px;
+  display: inline-flex;
+  align-items: center;
+  gap: 4px;
 }
 
-nav a.router-link-active {
+.navigation a.router-link-active {
   color: #42b983;
+}
+
+.nav-icon {
+  width: 16px;
+  height: 16px;
 }
 </style>
